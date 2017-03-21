721b99e38bd4a61f38bf72647bc9a83f2f676f9e
vkolodrevskiy <viktor.kolodrevskiy@gmail.com>
2017-03-21 15:30:40 +0200

    Adde main and logger.

---
+++ data-manipulation/java1/src/main/java/rubiconproject/App.java
@@ -0,0 +1,32 @@
+package rubiconproject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rubiconproject.service.SiteDataProcessor;
+
+import java.io.File;
+
+/**
+ * Main app.
+ *
+ * @author vkolodrevskiy
+ */
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+    private static final String OUTPUT_DEFAULT_FILE_NAME = "output.json";
+
+    public static void main(String[] args) {
+        if (args.length == 0 || isNullOrBlank(args[0])) {
+            log.error("No path to directory.");
+            return;
+        }
+        String outputFileName = (args.length > 1) ? args[1] : OUTPUT_DEFAULT_FILE_NAME;
+
+        SiteDataProcessor processor = new SiteDataProcessor();
+        processor.processFolder(new File(args[0]), new File(outputFileName));
+    }
+
+    private static boolean isNullOrBlank(String s) {
+        return (s==null || s.trim().equals(""));
+    }
+}
--- data-manipulation/java1/src/main/java/rubiconproject/service/SiteDataProcessor.java
+++ data-manipulation/java1/src/main/java/rubiconproject/service/SiteDataProcessor.java
@@ -1,5 +1,6 @@
 package rubiconproject.service;
 
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rubiconproject.model.Site;
@@ -9,7 +10,10 @@ import rubiconproject.reader.DataFileReader;
 import rubiconproject.reader.FileReaderFactory;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -23,26 +27,34 @@ public class SiteDataProcessor {
     private final FilesystemSourceListProvider sourceListProvider;
     private final KeywordService keywordService;
     private final FileReaderFactory fileReaderFactory;
+    private final Gson gson;
 
     public SiteDataProcessor() {
-        sourceListProvider = new FilesystemSourceListProvider();
-        keywordService = new DummyKeywordService();
-        fileReaderFactory = new FileReaderFactory();
+        this.sourceListProvider = new FilesystemSourceListProvider();
+        this.keywordService = new DummyKeywordService();
+        this.fileReaderFactory = new FileReaderFactory();
+        this.gson = new Gson();
     }
 
-    public void processFolder(File inputDirectory) {
-        List<Site> sites = new ArrayList<>();
+    public void processFolder(File inputDirectory, File output) {
         List<Source> sources = sourceListProvider.getInputSources(inputDirectory);
         for (Source source : sources) {
-            sites.addAll(readRawEntries(source));
+            processSource(source, output);
         }
-
-        // collections.addAll(sources.stream().map(source -> fileReaderFactory.getReader(source.getType())));
     }
 
-    private List<Site> readRawEntries(Source source) {
-        //DataFileReader reader = fileReaderFactory.getReader(source.getType());
-        //return reader.read(source.getFile());
-        return null;
+    private void processSource(Source source, File output) {
+        try (DataFileReader reader = fileReaderFactory.getReader(source.getType(), source.getFile());
+             PrintWriter pw = new PrintWriter(new FileWriter(output))) {
+            List<Site> sites = new LinkedList<>();
+            for (Site site : reader) {
+                site.setKeywords(keywordService.resolveKeywords(site));
+                sites.add(site);
+            }
+            SiteCollection collection = new SiteCollection(source.getFile().getName(), sites);
+            pw.println(gson.toJson(collection));
+        } catch (IOException e) {
+            log.error("Error while processing sites data.", e);
+        }
     }
 }
